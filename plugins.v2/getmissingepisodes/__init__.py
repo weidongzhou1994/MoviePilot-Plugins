47dd893bc8ed959faf89be18d1f6a535ddd97e3b
Weidong Zhou <sirzhouchn@163.com>
2026-02-01 17:38:04 +0800

    update

--- plugins.v2/getmissingepisodes/__init__.py
+++ plugins.v2/getmissingepisodes/__init__.py
@@ -176,7 +176,7 @@ class GetMissingEpisodes(_PluginBase):
     plugin_name = "剧集补全&新季追更"
     plugin_desc = "检测指定剧集库，对有新季或存在集缺失的剧集自动订阅补全"
     plugin_icon = "https://raw.githubusercontent.com/andyxu8023/MoviePilot-Plugins/main/icons/EpisodeNoExist.png"
-    plugin_version = "2.3.8"  # 更新版本号
+    plugin_version = "2.3.9"  # 更新版本号
     plugin_author = "boeto，左岸，qiniuweihe"
     author_url = "https://github.com/andyxu8023"
     plugin_config_prefix = "getmissingepisodes_"
@@ -939,7 +939,7 @@ class GetMissingEpisodes(_PluginBase):
         title_season = f"{title} ({year}) 第 {season} 季"
         logger.info(f"开始检查 {title_season} 是否已添加订阅")
 
-        extra_params = self._downOper.get_last_by(mtype=MediaType.TV, title=title, year=year, season=season, episode=None, tmdbid=tmdbid)
+        extra_params = self._downOper.get_last_by(mtype=MediaType.TV.value, title=title, year=year, season=season, episode=None, tmdbid=tmdbid)
         extra_params = extra_params[0].to_dict() if hasattr(extra_params[0], 'to_dict') else None
         logger.info(f"test-{extra_params}")
 
