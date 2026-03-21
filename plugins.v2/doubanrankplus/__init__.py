1ab4f6f101ebab352585bd1ace70bb069cde3635
Weidong Zhou <sirzhouchn@163.com>
2026-03-21 18:59:20 +0800

    update

--- plugins.v2/doubanrankplus/__init__.py
+++ plugins.v2/doubanrankplus/__init__.py
@@ -81,7 +81,7 @@ class DoubanRankPlus(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/boeto/MoviePilot-Plugins/main/icons/DouBanRankPlus.png"
     # 插件版本
-    plugin_version = "3.0.7"
+    plugin_version = "3.0.8"
     # 插件作者
     plugin_author = "boeto"
     # 作者主页
@@ -2018,7 +2018,8 @@ class DoubanRankPlus(_PluginBase):
                     ][0].to_dict()
                 else:
                     tmdbinfo = tmdbinfos[0].to_dict()
-                tmdbinfo["id"] = tmdbinfo["tmdb_id"]
+                tmdbinfo["id"] = tmdbinfo["tmdb_id"] 
+                tmdbinfo["media_type"] = MediaType.TV if tmdbinfo.get("type", "") == "电视剧" else MediaType.MOVIE 
                 logger.info(f"通过标题 {title} 和年份 {year} 获取到TMDB信息: {tmdbinfo}")
                 return tmdbinfo, False
             else:
